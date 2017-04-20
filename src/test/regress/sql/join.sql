04a9ac87f03607bef925ca715fa1d75ae7ab8c16
Tomas Vondra <tomas@2ndquadrant.com>
2017-04-21 00:41:13 +0200

    Resolve most failures in join regression tests
    This resolves most failures in 'join' regression suite. The expected
    results were produced by running the new join.sql script on XL 9.5,
    with a few tweaks of the explain plans (partial aggregates etc.)

--- src/test/regress/sql/join.sql
+++ src/test/regress/sql/join.sql
@@ -1613,18 +1613,21 @@ select x.* from
 select v.* from
   (int8_tbl x left join (select q1,coalesce(q2,0) q2 from int8_tbl) y on x.q2 = y.q1)
   left join int4_tbl z on z.f1 = x.q2,
-  lateral (select x.q1,y.q1 union all select x.q2,y.q2) v(vx,vy);
+  lateral (select x.q1,y.q1 union all select x.q2,y.q2) v(vx,vy)
+  order by vx, vy;
 select v.* from
   (int8_tbl x left join (select q1,(select coalesce(q2,0)) q2 from int8_tbl) y on x.q2 = y.q1)
   left join int4_tbl z on z.f1 = x.q2,
-  lateral (select x.q1,y.q1 union all select x.q2,y.q2) v(vx,vy);
+  lateral (select x.q1,y.q1 union all select x.q2,y.q2) v(vx,vy)
+  order by vx, vy;
 create temp table dual();
 insert into dual default values;
 analyze dual;
 select v.* from
   (int8_tbl x left join (select q1,(select coalesce(q2,0)) q2 from int8_tbl) y on x.q2 = y.q1)
   left join int4_tbl z on z.f1 = x.q2,
-  lateral (select x.q1,y.q1 from dual union all select x.q2,y.q2 from dual) v(vx,vy);
+  lateral (select x.q1,y.q1 from dual union all select x.q2,y.q2 from dual) v(vx,vy)
+  order by vx, vy;
 
 explain (verbose, num_nodes off, nodes off, costs off)
 select * from

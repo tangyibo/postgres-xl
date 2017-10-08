3a64cfdde398c79cc22ba9d3969965db9bc2605b
Tomas Vondra <tomas@2ndquadrant.com>
2017-10-08 23:44:27 +0200

    Stabilize combocid test by replicating the table
    Commit 1d14325822 randomized the choice of a starting node with
    ROUNDROBIN distribution, so that the data in combocid tests are not
    all placed on the first node but distributed randomly (despite using
    single-row INSERTS as before).
    
    So to stabilize the test, make the table replicated. The table only
    has a single column and the test updates is, so we can't use any
    other stable distribution (e.g. BY HASH).
    
    The expected results were obtained by running the combocid.sql on
    PostgreSQL 10, so there might be some cmin differences.

--- src/test/regress/sql/combocid.sql
+++ src/test/regress/sql/combocid.sql
@@ -1,7 +1,7 @@
 --
 -- Tests for some likely failure cases with combo cmin/cmax mechanism
 --
-CREATE TEMP TABLE combocidtest (foobar int) DISTRIBUTE BY ROUNDROBIN;
+CREATE TEMP TABLE combocidtest (foobar int) DISTRIBUTE BY REPLICATION;
 
 BEGIN;
 

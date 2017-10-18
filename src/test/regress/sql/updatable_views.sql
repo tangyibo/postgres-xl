1d3fd00e4de01ac34fae9f93febc92b6ba156648
Tomas Vondra <tomas@2ndquadrant.com>
2017-10-18 16:55:40 +0200

    Fix failures in updatable_views due to different structure
    Since commit 93cbab90b0c6fc3fc4aa515b93057127c0ee8a1b we enforce
    stricter rules on structure of partitioned tables, e.g. we do not
    allow different order of columns in parent/child tables.
    
    This was causing failures in the updatable_views tests, so fix that
    by ensuring the structure actually matches exactly.

--- src/test/regress/sql/updatable_views.sql
+++ src/test/regress/sql/updatable_views.sql
@@ -1115,12 +1115,12 @@ DROP TABLE t1;
 
 -- check that an auto-updatable view on a partitioned table works correctly
 create table pt (a int, b int, v varchar) partition by range (a, b);
-create table pt1 (b int not null, v varchar, a int not null) partition by range (b);
+create table pt1 (a int not null, b int not null, v varchar) partition by range (b);
 create table pt11 (like pt1);
-alter table pt11 drop a;
-alter table pt11 add a int;
-alter table pt11 drop a;
-alter table pt11 add a int not null;
+-- alter table pt11 drop a;
+-- alter table pt11 add a int;
+-- alter table pt11 drop a;
+-- alter table pt11 add a int not null;
 alter table pt1 attach partition pt11 for values from (2) to (5);
 alter table pt attach partition pt1 for values from (1, 2) to (1, 10);
 

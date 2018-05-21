4571eb88e16f9e1200ba3a00c0f73c705fecc864
Pavan Deolasee <pavan.deolasee@gmail.com>
2018-05-21 12:11:40 +0530

    Remove some accidentally added elog(LOG) messages

--- src/backend/commands/vacuum.c
+++ src/backend/commands/vacuum.c
@@ -1790,16 +1790,12 @@ get_remote_relstat(char *nspname, char *relname, bool istemp, bool replicated,
 		{
 			validpages++;
 			*pages += DatumGetInt32(value);
-			elog(LOG, "get_remote_relstat: relname:%s, remote relpages: %d",
-					relname, DatumGetInt32(value));
 		}
 		value = slot_getattr(result, 2, &isnull); /* reltuples */
 		if (!isnull)
 		{
 			validtuples++;
 			*tuples += DatumGetFloat4(value);
-			elog(LOG, "get_remote_relstat: relname:%s, remote reltuples: %f",
-					relname, DatumGetFloat4(value));
 		}
 		value = slot_getattr(result, 3, &isnull); /* relallvisible */
 		if (!isnull)
@@ -1890,8 +1886,6 @@ vacuum_rel_coordinator(Relation onerel, bool is_outer)
 	relname = RelationGetRelationName(onerel);
 	nspname = get_namespace_name(RelationGetNamespace(onerel));
 	istemp = (onerel->rd_rel->relpersistence == RELPERSISTENCE_TEMP);
-
-	elog(LOG, "Getting relation statistics for %s.%s", nspname, relname);
 
 	replicated = IsLocatorReplicated(RelationGetLocatorType(onerel));
 	/*

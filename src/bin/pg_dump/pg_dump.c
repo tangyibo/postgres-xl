tangyibo/postgres-xl

commit 0e3770c14c3fb1858192feb7240343cb35ba013c

Ensure qualified name for dumping sequence value
Without that the sequence won't be found correctly.

src/bin/pg_dump/pg_dump.c
@@ -16759,18 +16759,15 @@ dumpSequenceData(Archive *fout, TableDataInfo *tdinfo)
 		 */
 		resetPQExpBuffer(query);
 		appendPQExpBufferStr(query, "SELECT pg_catalog.nextval(");
-		appendStringLiteralAH(query, fmtId(tbinfo->dobj.name), fout);
+		appendStringLiteralAH(query, fmtQualifiedDumpable(tbinfo), fout);
 		appendPQExpBuffer(query, ");\n");
 		res = ExecuteSqlQuery(fout, query->data, PGRES_TUPLES_OK);
 
 		if (PQntuples(res) != 1)
 		{
-			write_msg(NULL, ngettext("query to get nextval of sequence \"%s\" "
-									 "returned %d rows (expected 1)\n",
-										"query to get nextval of sequence \"%s\" "
-									 "returned %d rows (expected 1)\n",
-									 PQntuples(res)),
-					tbinfo->dobj.name, PQntuples(res));
+			write_msg(NULL, "query to get nextval of sequence \"%s\" "
+							"returned %d rows (expected 1)\n",
+							fmtQualifiedDumpable(tbinfo), PQntuples(res));
 			exit_nicely(1);
 		}
 

commit 8c9e9a36f9be336c891436c5b4bd4f2eeb30619d

Fix compilation error introduced in 0e3770c14c3fb1858192feb7240343cb35ba013c
Per report and patch by Wanglin

src/bin/pg_dump/pg_dump.c
@@ -16762,7 +16762,7 @@ dumpSequenceData(Archive *fout, TableDataInfo *tdinfo)
 		 */
 		resetPQExpBuffer(query);
 		appendPQExpBufferStr(query, "SELECT pg_catalog.nextval(");
-		appendStringLiteralAH(query, fmtQualifiedDumpable(tbinfo), fout);
+		appendStringLiteralAH(query, fmtId(tbinfo->dobj.name), fout);
 		appendPQExpBuffer(query, ");\n");
 		res = ExecuteSqlQuery(fout, query->data, PGRES_TUPLES_OK);
 
@@ -16770,7 +16770,7 @@ dumpSequenceData(Archive *fout, TableDataInfo *tdinfo)
 		{
 			write_msg(NULL, "query to get nextval of sequence \"%s\" "
 							"returned %d rows (expected 1)\n",
-							fmtQualifiedDumpable(tbinfo), PQntuples(res));
+							fmtId(tbinfo->dobj.name), PQntuples(res));
 			exit_nicely(1);
 		}
 